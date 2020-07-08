0e7823e63e4d9faec8c1e986ffa4056d155ff94c
Brittny Rosenow <brittnyrosenow@gmail.com>
2020-07-07 18:28:43 -0600

    updated java

--- script.js
+++ script.js
@@ -37,7 +37,19 @@ function searchWeather(city) {
         dataType: "json",
         success: function(response){
             console.log(response);
-
+    $("#forecast").html("<h4 class=\"mt-3\">5-Day Forecast:</h4>").append("<div class=\"row\">");            
+            for (i=0; i < response.list.length; i++) {
+                if (response.list[i].dt_txt.indexOf("15:00:00")!== -1) {
+                    var col = $("<div>").addClass("col-md-2");
+                    var card = $("<div>").addClass("card bg-primary text-white");
+                    var body = $("<div>").addClass("card-body p-2");
+                    var temp = $("<p>").addClass("card-text").text("temp:" + response.list[i].main.temp_max);
+                    var humid = $("<p>").addClass("card-text").text("humidity:" + response.list[i].main.humidity);
+                    //if want icon and titleDate they would go here
+                    col.append(card.append(body.append(temp, humid)));
+                    $("#forecast .row").append(col);
+                }
+            }
 }})}
 
 })
